4be8114253b99b743cf3b90a20ee2560e59b898f
Ekaterina Sviridova <sviridova.1996@gmail.com>
2025-10-08 12:32:43 +0200

    updated

--- 7B_Mistral_Llama/finetune_binary.py
+++ 7B_Mistral_Llama/finetune_binary.py
@@ -25,7 +25,7 @@ nltk.download("punkt_tab")
 
 # Configure logging
 def setup_logging(model_name):
-    log_filename = f"{model_name}_finetune_binary.log"
+    log_filename = f"{model_name}_micro_finetune_binary.log"
     logging.basicConfig(
         filename=log_filename,
         level=logging.INFO,
@@ -190,7 +190,7 @@ def parse_args():
                        default='mistral-7b',
                        help='Model to finetune: mistral-7b or llama-8b')
     parser.add_argument('--data_dir', type=str, 
-                       default='out_jsonl',
+                       default='out_combined_binary_jsonl',
                        help='Directory with train.jsonl, dev.jsonl, test.jsonl')
     parser.add_argument('--output_dir', type=str,
                        default=None,
